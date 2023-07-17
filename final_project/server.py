da52c2d55c784df129e1423c4fa5b15f534ed996
Saintshack <tpatel072022@gmail.com>
2023-07-16 21:39:42 -0500

    final commit

--- final_project/server.py
+++ final_project/server.py
@@ -1,24 +1,23 @@
-from machinetranslation import translator
 from flask import Flask, render_template, request
-import json
+from translator import englishToFrench, frenchToEnglish
 
 app = Flask("Web Translator")
 
 @app.route("/englishToFrench")
-def englishToFrench():
+def englishtoFrench():
     textToTranslate = request.args.get('textToTranslate')
-    # Write your code here
-    return "Translated text to French"
+    NewText= englishToFrench(textToTranslate)
+    return (NewText)
 
 @app.route("/frenchToEnglish")
-def frenchToEnglish():
+def frenchtoEnglish():
     textToTranslate = request.args.get('textToTranslate')
-    # Write your code here
-    return "Translated text to English"
+    NewText = frenchToEnglish(textToTranslate)
+    return (NewText)
 
 @app.route("/")
 def renderIndexPage():
-    # Write the code to render template
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
--- machinetranslation/tests.py
+++ machinetranslation/tests.py
@@ -1,6 +1,6 @@
 import unittest
 
-from translator import englishToFrench, FrenchToEnglish
+from translator import englishToFrench, frenchToEnglish
 
 
 class Testtranslation(unittest.TestCase):
@@ -8,8 +8,8 @@ class Testtranslation(unittest.TestCase):
         self.assertEqual(englishToFrench("hello"), "bonjour")  # test when hello is given as input the output is bonjour.
         self.assertNotEqual(englishToFrench("Hello"), "bonjour")
     def test2(self):
-        self.assertEqual(FrenchToEnglish("bonjour"), "hello") # test when bonjour is given as input the output is hello.
-        self.assertNotEqual(FrenchToEnglish("onjour"), "hello")
+        self.assertEqual(frenchToEnglish("bonjour"), "hello") # test when bonjour is given as input the output is hello.
+        self.assertNotEqual(frenchToEnglish("onjour"), "hello")
 
 if __name__ == '__Main__':
     unittest.main()
